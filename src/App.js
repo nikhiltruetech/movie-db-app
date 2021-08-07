d41258ef76defd3f60b903ab13cd4796b555e013
itsoftware <nikhil@truetechpro.com>
2021-08-07 11:32:27 +0530

    Production ready

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { Router, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch,Redirect } from "react-router-dom";
 import Home from "./Components/Home";
 import MovieDetails from "./Components/MovieDetails";
 import { history } from "./config/history";
-import Header from './Layout/Header'
+import Header from "./Layout/Header";
 const App = () => {
   return (
     <Router history={history}>
       <Header />
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/movie-details" component={MovieDetails}/>
+        <Route exact path="/">
+          <Redirect to="/movie-db-app.github.io" />
+        </Route>
+        <Route exact path="/movie-db-app.github.io" component={Home} />
+        <Route exact path="/movie-details" component={MovieDetails} />
       </Switch>
     </Router>
   );
